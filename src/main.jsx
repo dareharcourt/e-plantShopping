86a1293a2a3962244cc6fd551399dc849652acef
Dare Harcourt <dareharcourt@Dares-MacBook-Air.local>
2026-02-04 18:58:24 -0600

    Prepare for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // Import Provider from react-redux
+import store from './store.js'; // Import the Redux store
+import App from './App';
+import './index.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap the entire app with Provider and pass the store */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
