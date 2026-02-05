13d53f470010c960e0e93ed07fe51b3e2bf6f8d2
Dare Harcourt <dareharcourt@Dares-MacBook-Air.local>
2026-02-04 18:51:48 -0600

    Add Redux store configuration

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
-});
-export default store
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
